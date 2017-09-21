5576abd386dec113ebb0c61d75984443b7e19ccb
Southclaws <Southclaws@gmail.com>
2017-09-21 19:02:48 +0100

    implemented download command

--- main.go
+++ main.go
@@ -4,35 +4,37 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/pkg/errors"
 	"gopkg.in/urfave/cli.v1"
 )
 
 func main() {
-	// load settings from json, env or cmd (cobra/viper?)
-	// generate server.cfg in working dir
-	// launch server as child process
-	// monitor child process for non-zero exits
-	// pipe stdout to custom log handler
-
 	app := cli.NewApp()
 
-	app.Version = "0.1.0"
 	app.Author = "Southclaws"
 	app.Email = "southclaws@gmail.com"
 	app.Name = "sampctl"
 
-	fmt.Println("sampctl")
+	cli.VersionFlag = cli.BoolFlag{
+		Name:  "app-version, V",
+		Usage: "show the app version number",
+	}
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:  "ver",
-			Value: "samp037svr_R2-2-1",
+			Name:  "version",
+			Value: "0.3.7",
 			Usage: "server version - corresponds to http://files.sa-mp.com packages without the .tar.gz",
 		},
 		cli.StringFlag{
 			Name:  "dir",
 			Value: ".",
 			Usage: "working directory for the server - by default, uses the current directory",
+		},
+		cli.StringFlag{
+			Name:  "endpoint",
+			Value: "http://files.sa-mp.com",
+			Usage: "endpoint to download packages from",
 		},
 	}
 
@@ -42,25 +44,45 @@ func main() {
 			Aliases: []string{"r"},
 			Usage:   "run a sa-mp server, uses the cwd if --dir is not set",
 			Action: func(c *cli.Context) error {
-				// check if sa-mp server present in current directory or --dir path
-				// if not, run the "download" route first
-				// then run the server as normal
+				endpoint := c.String("endpoint")
+				ver := c.String("ver")
+				dir := c.String("dir")
+				err := GetPackage(endpoint, ver, dir)
+				if err != nil {
+					return errors.Wrap(err, "failed to get package")
+				}
+				// run from dir
 				return nil
 			},
+			Flags: app.Flags,
 		},
 		{
 			Name:    "download",
 			Aliases: []string{"d"},
-			Usage:   "download a version of the server, uses latest if --ver is not specified",
+			Usage:   "download a version of the server, uses latest if --version is not specified",
 			Action: func(c *cli.Context) error {
-				// --ver refers to a filename on http://files.sa-mp.com without the extension
-				// samp037svr_R2-2-1 maps to http://files.sa-mp.com/samp037svr_R2-2-1.tar.gz etc
-				return nil
+				endpoint := c.String("endpoint")
+				ver := c.String("ver")
+				dir := c.String("dir")
+				fmt.Printf("Downloading package %s from endpoint %s into %s\n", ver, endpoint, dir)
+				return GetPackage(endpoint, ver, dir)
 			},
+			Flags: app.Flags,
 		},
 	}
 
-	err := app.Run(os.Args)
+	cacheDir, err := getCacheDir()
+	if err != nil {
+		fmt.Println("Failed to retrieve cache directory path (attempted <user folder>/.samp) ", err)
+		return
+	}
+	err = os.MkdirAll(cacheDir, 0665)
+	if err != nil {
+		fmt.Println("Failed to create cache directory at ", cacheDir, ": ", err)
+		return
+	}
+
+	err = app.Run(os.Args)
 	if err != nil {
 		fmt.Printf("Exited with error: %v\n", err)
 	}
