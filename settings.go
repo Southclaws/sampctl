0b0d7f286726a899d3024c838d81ec4325a0869d
Barnaby <barnaby@spotlightdata.co.uk>
2017-09-25 11:06:22 +0100

    organised settings into categories, fixed bad file descriptor, removed `gamemode` to resolve #8

--- settings.go
+++ settings.go
@@ -15,47 +15,51 @@ import (
 
 // Config stores the server settings and working directory
 type Config struct {
-	Gamemodes         []string `                        json:"gamemodes,omitempty" cfg:"gamemode" numbered:"1"` //
-	Gamemode          *string  `                        json:"gamemode,omitempty" cfg:"gamemode0"`              //
-	RCONPassword      *string  `required:"1"            json:"rcon_password,omitempty"`                         // changeme
-	Announce          *bool    `default:"0"             required:"0" json:"announce,omitempty"`                 // 0
-	MaxPlayers        *int     `default:"50"            required:"0" json:"maxplayers,omitempty"`               // 50
-	Port              *int     `default:"8192"          required:"0" json:"port,omitempty"`                     // 8192
-	LANMode           *bool    `default:"0"             required:"0" json:"lanmode,omitempty"`                  // 0
-	Query             *bool    `default:"0"             required:"0" json:"query,omitempty"`                    // 0
-	RCON              *bool    `default:"0"             required:"0" json:"rcon,omitempty"`                     // 0
-	LogQueries        *bool    `default:"0"             required:"0" json:"logqueries,omitempty"`               // 0
-	StreamRate        *int     `default:"1000"          required:"0" json:"stream_rate,omitempty"`              // 1000
-	StreamDistance    *float32 `default:"200.0"         required:"0" json:"stream_distance,omitempty"`          // 200.0
-	Sleep             *string  `default:"5"             required:"0" json:"sleep,omitempty"`                    // 5
-	MaxNPC            *int     `default:"0"             required:"0" json:"maxnpc,omitempty"`                   // 0
-	OnFootRate        *int     `default:"30"            required:"0" json:"onfoot_rate,omitempty"`              // 30
-	InCarRate         *int     `default:"30"            required:"0" json:"incar_rate,omitempty"`               // 30
-	WeaponRate        *int     `default:"30"            required:"0" json:"weapon_rate,omitempty"`              // 30
-	ChatLogging       *bool    `default:"1"             required:"0" json:"chatlogging,omitempty"`              // 1
-	Timestamp         *bool    `default:"1"             required:"0" json:"timestamp,omitempty"`                // 1
-	Bind              *string  `                        required:"0" json:"bind,omitempty"`                     //
-	Password          *string  `                        required:"0" json:"password,omitempty"`                 //
-	Hostname          *string  `default:"SA-MP Server"  required:"0" json:"hostname,omitempty"`                 // SA-MP Server
-	Language          *string  `default:"-"             required:"0" json:"language,omitempty"`                 //
-	Mapname           *string  `default:"San Andreas"   required:"0" json:"mapname,omitempty"`                  // San Andreas
-	Weburl            *string  `default:"www.sa-mp.com" required:"0" json:"weburl,omitempty"`                   // www.sa-mp.com
-	GamemodeText      *string  `default:"Unknown"       required:"0" json:"gamemodetext,omitempty"`             // Unknown
-	Filterscripts     []string `                        required:"0" json:"filterscripts,omitempty"`            //
-	Plugins           []string `                        required:"0" json:"plugins,omitempty"`                  //
-	NoSign            *string  `                        required:"0" json:"nosign,omitempty"`                   //
-	LogTimeFormat     *string  `default:"[%H:%M:%S]"    required:"0" json:"logtimeformat,omitempty"`            // [%H:%M:%S]
-	MessageHoleLimit  *int     `default:"3000"          required:"0" json:"messageholelimit,omitempty"`         // 3000
-	MessagesLimit     *int     `default:"500"           required:"0" json:"messageslimit,omitempty"`            // 500
-	AcksLimit         *int     `default:"3000"          required:"0" json:"ackslimit,omitempty"`                // 3000
-	PlayerTimeout     *int     `default:"10000"         required:"0" json:"playertimeout,omitempty"`            // 10000
-	MinConnectionTime *int     `default:"0"             required:"0" json:"minconnectiontime,omitempty"`        // 0
-	LagCompmode       *int     `default:"1"             required:"0" json:"lagcompmode,omitempty"`              // 1
-	ConnseedTime      *int     `default:"300000"        required:"0" json:"connseedtime,omitempty"`             // 300000
-	DBLogging         *bool    `default:"0"             required:"0" json:"db_logging,omitempty"`               // 0
-	DBLogQueries      *bool    `default:"0"             required:"0" json:"db_log_queries,omitempty"`           // 0
-	ConnectCookies    *bool    `default:"1"             required:"0" json:"conncookies,omitempty"`              // 1
-	CookieLogging     *bool    `default:"0"             required:"0" json:"cookielogging,omitempty"`            // 1
+	// Core properties
+	Gamemodes     []string `                        json:"gamemodes,omitempty" cfg:"gamemode" numbered:"1"` //
+	Filterscripts []string `                        required:"0" json:"filterscripts,omitempty"`            //
+	Plugins       []string `                        required:"0" json:"plugins,omitempty"`                  //
+	RCONPassword  *string  `required:"1"            json:"rcon_password,omitempty"`                         // changeme
+	Port          *int     `default:"8192"          required:"0" json:"port,omitempty"`                     // 8192
+	Hostname      *string  `default:"SA-MP Server"  required:"0" json:"hostname,omitempty"`                 // SA-MP Server
+	MaxPlayers    *int     `default:"50"            required:"0" json:"maxplayers,omitempty"`               // 50
+	Language      *string  `default:"-"             required:"0" json:"language,omitempty"`                 //
+	Mapname       *string  `default:"San Andreas"   required:"0" json:"mapname,omitempty"`                  // San Andreas
+	Weburl        *string  `default:"www.sa-mp.com" required:"0" json:"weburl,omitempty"`                   // www.sa-mp.com
+	GamemodeText  *string  `default:"Unknown"       required:"0" json:"gamemodetext,omitempty"`             // Unknown
+
+	// Network and technical config
+	Bind       *string `                        required:"0" json:"bind,omitempty"`       //
+	Password   *string `                        required:"0" json:"password,omitempty"`   //
+	Announce   *bool   `default:"0"             required:"0" json:"announce,omitempty"`   // 0
+	LANMode    *bool   `default:"0"             required:"0" json:"lanmode,omitempty"`    // 0
+	Query      *bool   `default:"0"             required:"0" json:"query,omitempty"`      // 0
+	RCON       *bool   `default:"0"             required:"0" json:"rcon,omitempty"`       // 0
+	LogQueries *bool   `default:"0"             required:"0" json:"logqueries,omitempty"` // 0
+	Sleep      *string `default:"5"             required:"0" json:"sleep,omitempty"`      // 5
+	MaxNPC     *int    `default:"0"             required:"0" json:"maxnpc,omitempty"`     // 0
+
+	// Rates and performance
+	StreamRate        *int     `default:"1000"          required:"0" json:"stream_rate,omitempty"`       // 1000
+	StreamDistance    *float32 `default:"200.0"         required:"0" json:"stream_distance,omitempty"`   // 200.0
+	OnFootRate        *int     `default:"30"            required:"0" json:"onfoot_rate,omitempty"`       // 30
+	InCarRate         *int     `default:"30"            required:"0" json:"incar_rate,omitempty"`        // 30
+	WeaponRate        *int     `default:"30"            required:"0" json:"weapon_rate,omitempty"`       // 30
+	ChatLogging       *bool    `default:"1"             required:"0" json:"chatlogging,omitempty"`       // 1
+	Timestamp         *bool    `default:"1"             required:"0" json:"timestamp,omitempty"`         // 1
+	NoSign            *string  `                        required:"0" json:"nosign,omitempty"`            //
+	LogTimeFormat     *string  `default:"[%H:%M:%S]"    required:"0" json:"logtimeformat,omitempty"`     // [%H:%M:%S]
+	MessageHoleLimit  *int     `default:"3000"          required:"0" json:"messageholelimit,omitempty"`  // 3000
+	MessagesLimit     *int     `default:"500"           required:"0" json:"messageslimit,omitempty"`     // 500
+	AcksLimit         *int     `default:"3000"          required:"0" json:"ackslimit,omitempty"`         // 3000
+	PlayerTimeout     *int     `default:"10000"         required:"0" json:"playertimeout,omitempty"`     // 10000
+	MinConnectionTime *int     `default:"0"             required:"0" json:"minconnectiontime,omitempty"` // 0
+	LagCompmode       *int     `default:"1"             required:"0" json:"lagcompmode,omitempty"`       // 1
+	ConnseedTime      *int     `default:"300000"        required:"0" json:"connseedtime,omitempty"`      // 300000
+	DBLogging         *bool    `default:"0"             required:"0" json:"db_logging,omitempty"`        // 0
+	DBLogQueries      *bool    `default:"0"             required:"0" json:"db_log_queries,omitempty"`    // 0
+	ConnectCookies    *bool    `default:"1"             required:"0" json:"conncookies,omitempty"`       // 1
+	CookieLogging     *bool    `default:"0"             required:"0" json:"cookielogging,omitempty"`     // 1
 }
 
 // NewConfigFromEnvironment creates a Config from the given environment which includes a directory which
@@ -326,7 +330,7 @@ func fromFloat(name string, obj reflect.Value, required bool, defaultValue strin
 func (cfg Config) GenerateJSON(dir string) (err error) {
 	path := filepath.Join(dir, "samp.json")
 
-	fh, err := os.OpenFile(path, os.O_CREATE, 0755)
+	fh, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0755)
 	if err != nil {
 		return
 	}
@@ -342,6 +346,6 @@ func (cfg Config) GenerateJSON(dir string) (err error) {
 		return
 	}
 
-	_, err = fh.WriteAt(contents, 0)
+	_, err = fh.Write(contents)
 	return
 }
--- settings_test.go
+++ settings_test.go
@@ -1,6 +1,12 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestServer_GenerateServerCfg(t *testing.T) {
 	type args struct {
@@ -15,12 +21,12 @@ func TestServer_GenerateServerCfg(t *testing.T) {
 		{
 			"required",
 			&Config{
-				// Announce:     &[]bool{true}[0],
-				// Hostname:     &[]string{"Test"}[0],
-				// MaxPlayers:   &[]int{32}[0],
-				// Port:         &[]int{8080}[0],
-				// RCON:         &[]bool{true}[0],
-				// Language:     &[]string{"English"}[0],
+				Announce:   &[]bool{true}[0],
+				Hostname:   &[]string{"Test"}[0],
+				MaxPlayers: &[]int{32}[0],
+				Port:       &[]int{8080}[0],
+				RCON:       &[]bool{true}[0],
+				Language:   &[]string{"English"}[0],
 				Gamemodes: []string{
 					"rivershell",
 					"baserace",
@@ -36,6 +42,62 @@ func TestServer_GenerateServerCfg(t *testing.T) {
 			if err := tt.cfg.GenerateServerCfg(tt.args.path); (err != nil) != tt.wantErr {
 				t.Errorf("Config.GenerateServerCfg() error = %v, wantErr %v", err, tt.wantErr)
 			}
+		})
+	}
+}
+
+func TestConfig_GenerateJSON(t *testing.T) {
+	type args struct {
+		dir string
+	}
+	tests := []struct {
+		name    string
+		config  *Config
+		want    []byte
+		args    args
+		wantErr bool
+	}{
+		{
+			"minimal",
+			&Config{
+				Gamemodes: []string{
+					"rivershell",
+					"baserace",
+				},
+				RCONPassword: &[]string{"test"}[0],
+				Port:         &[]int{8080}[0],
+				Hostname:     &[]string{"Test"}[0],
+				MaxPlayers:   &[]int{32}[0],
+				Language:     &[]string{"English"}[0],
+				Announce:     &[]bool{true}[0],
+				RCON:         &[]bool{true}[0],
+			},
+			[]byte(`{
+	"gamemodes": [
+		"rivershell",
+		"baserace"
+	],
+	"rcon_password": "test",
+	"port": 8080,
+	"hostname": "Test",
+	"maxplayers": 32,
+	"language": "English",
+	"announce": true,
+	"rcon": true
+}`),
+			args{"./testspace"},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.config.GenerateJSON(tt.args.dir)
+			assert.NoError(t, err)
+
+			contents, err := ioutil.ReadFile(filepath.Join(tt.args.dir, "samp.json"))
+			assert.NoError(t, err)
+
+			assert.Equal(t, string(tt.want), string(contents))
 		})
 	}
 }
