173bfb4169cd82a15e8485745ce751330000a94c
Southclaws <SouthcawJK@gmail.com>
2017-10-20 10:16:01 +0100

    shfmt linux-install

--- linux-install.sh
+++ linux-install.sh
@@ -4,15 +4,14 @@
 ARCH=$(uname -p)
 PATTERN="browser_download_url.*386\.deb"
 
-if [ $ARCH = "x86_64" ]
-then
-    PATTERN="browser_download_url.*amd64\.deb"
+if [ $ARCH = "x86_64" ]; then
+	PATTERN="browser_download_url.*amd64\.deb"
 fi
 
-curl -s https://api.github.com/repos/Southclaws/sampctl/releases/latest \
-| grep $PATTERN \
-| cut -d : -f 2,3 \
-| tr -d \" \
-| wget -qi - -O tmp.deb
+curl -s https://api.github.com/repos/Southclaws/sampctl/releases/latest |
+	grep $PATTERN |
+	cut -d : -f 2,3 |
+	tr -d \" |
+	wget -qi - -O tmp.deb
 sudo dpkg -i tmp.deb
 rm tmp.deb
