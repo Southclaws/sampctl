205b0921cdeb1ccd8802a1c24872669ab5430daf
Southclaws <southclawjk@gmail.com>
2017-11-02 14:40:17 +0000

    written a load test for yaml

--- rook/load_test.go
+++ rook/load_test.go
@@ -1,8 +1,9 @@
 package rook
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPackageFromDir(t *testing.T) {
@@ -16,6 +17,21 @@ func TestPackageFromDir(t *testing.T) {
 		wantErr bool
 	}{
 		{"build-json", args{"./tests/build-json"}, Package{
+			local:  "./tests/build-json",
+			Entry:  "gamemodes/test.pwn",
+			Output: "gamemodes/test.amx",
+			Dependencies: []Dependency{
+				Dependency("Southclaws/samp-stdlib:0.3.7-R2-2-1"),
+				Dependency("Southclaws/SIF:1.6.2"),
+				Dependency("Misiur/YSI-Includes"),
+				Dependency("samp-incognito/samp-streamer-plugin:2.9.1"),
+				Dependency("Zeex/amx_assembly"),
+			}},
+			false},
+		{"build-yaml", args{"./tests/build-yaml"}, Package{
+			local:  "./tests/build-yaml",
+			Entry:  "gamemodes/test.pwn",
+			Output: "gamemodes/test.amx",
 			Dependencies: []Dependency{
 				Dependency("Southclaws/samp-stdlib:0.3.7-R2-2-1"),
 				Dependency("Southclaws/SIF:1.6.2"),
@@ -28,13 +44,13 @@ func TestPackageFromDir(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			gotPkg, err := PackageFromDir(tt.args.dir)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("PackageFromDir() error = %v, wantErr %v", err, tt.wantErr)
-				return
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
 			}
-			if !reflect.DeepEqual(gotPkg, tt.wantPkg) {
-				t.Errorf("PackageFromDir() = %v, want %v", gotPkg, tt.wantPkg)
-			}
+
+			assert.Equal(t, tt.wantPkg, gotPkg)
 		})
 	}
 }
---
+++ rook/tests/build-yaml/gamemodes/test.pwn
@@ -0,0 +1,5 @@
+#include <SIF/Item.pwn>
+
+main() {
+    print("hi");
+}
