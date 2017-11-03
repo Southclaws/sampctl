c3cd4fc73fac10ab0f7c74352cbbfe826661e2b4
Southclaws <southclawjk@gmail.com>
2017-11-03 21:09:26 +0000

    updated go git and other deps, fixed some small issues with checkouts

--- rook/ensure.go
+++ rook/ensure.go
@@ -3,8 +3,10 @@ package rook
 import (
 	"fmt"
 	"path/filepath"
+	"sort"
 
 	"github.com/Masterminds/semver"
+	"github.com/Southclaws/sampctl/util"
 	"github.com/pkg/errors"
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing"
@@ -16,20 +18,24 @@ import (
 // If the package is not present, it will clone it at the correct version tag, sha1 or HEAD
 // If the package is present, it will ensure the directory contains the correct version
 func EnsurePackage(vendorDirectory string, pkg Package) (err error) {
-	pkgPath := filepath.Join(vendorDirectory, pkg.repo)
+	pkgPath := filepath.Join(util.FullPath(vendorDirectory), pkg.repo)
 
 	repo, err := git.PlainOpen(pkgPath)
-	if err == git.ErrRepositoryNotExists {
-		fmt.Println(pkg, "package does not exist, cloning new copy")
+	if err != nil {
+		if err == git.ErrRepositoryNotExists {
+			fmt.Println(pkg, "package does not exist at", pkgPath, "cloning new copy")
 
-		repo, err = git.PlainClone(pkgPath, false, &git.CloneOptions{
-			URL: pkg.GetURL(),
-		})
-		if err != nil {
+			repo, err = git.PlainClone(pkgPath, false, &git.CloneOptions{
+				URL: pkg.GetURL(),
+			})
+			if err != nil {
+				err = errors.Wrap(err, "failed to clone dependency repository")
+				return
+			}
+		} else {
+			err = errors.Wrap(err, "failed to open dependency repository")
 			return
 		}
-	} else if err != nil {
-		return
 	} else {
 		fmt.Println(pkg, "package already exists, ensuring local copy matches version constraint")
 	}
@@ -50,37 +56,54 @@ func EnsurePackage(vendorDirectory string, pkg Package) (err error) {
 	constraint, err := semver.NewConstraint(pkg.version)
 	if err != nil {
 		// todo: support non-semver versioning by just using tag
-		return errors.Wrapf(err, "%s package version constraint is not valid", pkg)
+		return errors.Wrap(err, "package version constraint is not valid")
 	}
 
 	tags, err := repo.Tags()
 	if err != nil {
-		return
+		return errors.Wrap(err, "failed to get repo tags")
 	}
 	defer tags.Close()
 
 	var ref *plumbing.Reference
+	versionedTags := semver.Collection{}
+	allRefs := make(map[string]*plumbing.Reference)
 	tags.ForEach(func(pr *plumbing.Reference) error {
-		tagVersion, err := semver.NewVersion(pr.Name().Short())
+		tag := pr.Name().Short()
+
+		tagVersion, err := semver.NewVersion(tag)
 		if err != nil {
-			fmt.Println("tag %s is not a valid semver!", pr.Name().Short())
 			return nil
 		}
 
-		if constraint.Check(tagVersion) {
-			ref = pr
-		}
+		versionedTags = append(versionedTags, tagVersion)
+		allRefs[tagVersion.String()] = pr
 
 		return nil
 	})
 
+	sort.Sort(sort.Reverse(versionedTags))
+
+	for _, version := range versionedTags {
+		tag := allRefs[version.String()]
+
+		if constraint.Check(version) {
+			fmt.Println(pkg, "discovered tag", tag, "that matches constraint", pkg.version, tag.Hash().String())
+			ref = tag
+			break
+		}
+
+		fmt.Println(pkg, "incompatible tag", tag, "does not satisfy constraint", pkg.version, tag.Hash().String())
+	}
+
 	if ref == nil {
-		err = errors.Errorf("failed to satisfy constraint for %s, no tag found by that name", pkg)
+		err = errors.Errorf("failed to satisfy constraint, no tag found by that name, available tags: %v", versionedTags)
 		return
 	}
 
 	wt, err := repo.Worktree()
 	if err != nil {
+		err = errors.Wrap(err, "failed to get repo worktree")
 		return
 	}
 
@@ -89,8 +112,12 @@ func EnsurePackage(vendorDirectory string, pkg Package) (err error) {
 		Force: true,
 	})
 	if err != nil {
+		err = errors.Wrapf(err, "failed to checkout necessary commit %s", ref.Hash())
 		return
 	}
+
+	head, _ := repo.Head()
+	fmt.Println(pkg, "successfully checked out to", head.Hash().String())
 
 	return
 }
--- rook/ensure_test.go
+++ rook/ensure_test.go
@@ -10,10 +10,10 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	os.MkdirAll("./tests", 0755)
+	os.MkdirAll("./tests/deps", 0755)
 
-	// Make sure our test dir is empty before running tests
-	err := os.RemoveAll("./tests/SIF*")
+	// Make sure our ensure tests dir is empty before running tests
+	err := os.RemoveAll("./tests/deps")
 	if err != nil {
 		panic(err)
 	}
@@ -33,25 +33,24 @@ func TestEnsurePackage(t *testing.T) {
 		wantErr bool
 		delete  bool
 	}{
-		{"SIF latest", args{"./tests", Package{
-			user: "Southclaws",
-			repo: "SIF",
-		}}, "b1db5430428fe89f1cdbcb8267fe8f9f9b78df92", false, false},
-		{"SIF 1.6.0", args{"./tests", Package{
-			user:    "Southclaws",
-			repo:    "SIF",
-			version: "1.6.0",
-		}}, "0693d8e85fd8b41a225912d26b2455449a6965a0", false, true},
-		{"SIF 1.6.0", args{"./tests", Package{
-			user:    "Southclaws",
-			repo:    "SIF",
-			version: "1.6.0",
-		}}, "0693d8e85fd8b41a225912d26b2455449a6965a0", false, true},
-		{"SIF 1.3.x", args{"./tests", Package{
+		// {"SIF latest", args{"./tests/deps", Package{
+		// 	user: "Southclaws",
+		// 	repo: "SIF",
+		// }}, "b1db5430428fe89f1cdbcb8267fe8f9f9b78df92", false, true},
+		{"SIF 1.3.x", args{"./tests/deps", Package{
 			user:    "Southclaws",
 			repo:    "SIF",
 			version: "1.3.x",
-		}}, "433fc17e9c6bf66bdf7ef3b82b70eea1c34af43f", false, true},
+		}}, "433fc17e9c6bf66bdf7ef3b82b70eea1c34af43f", false, false},
+		// {"crashdetect latest", args{"./tests/deps", Package{
+		// 	user: "Zeex",
+		// 	repo: "samp-plugin-crashdetect",
+		// }}, "722f3e80e47b74ff694fd1805b9d2922c2c15ce0", false, true},
+		// {"crashdetect 4.15", args{"./tests/deps", Package{
+		// 	user:    "Zeex",
+		// 	repo:    "samp-plugin-crashdetect",
+		// 	version: "4.15",
+		// }}, "b4d4abd02bfc7052a73af27ca5d828c8ec057481", false, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -62,13 +61,13 @@ func TestEnsurePackage(t *testing.T) {
 				assert.NoError(t, err)
 			}
 
-			repo, _ := git.PlainOpen(filepath.Join("./tests", tt.args.pkg.repo)) //nolint
+			repo, _ := git.PlainOpen(filepath.Join("./tests/deps", tt.args.pkg.repo)) //nolint
 			ref, _ := repo.Head()
 			assert.Equal(t, tt.wantSha, ref.Hash().String())
 
 			// cleanup
 			if tt.delete {
-				err = os.RemoveAll(filepath.Join("./tests", tt.args.pkg.repo))
+				err = os.RemoveAll(filepath.Join("./tests/deps", tt.args.pkg.repo))
 				if err != nil {
 					panic(err)
 				}
