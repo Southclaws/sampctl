ecf7665688bd37907ddce704f7cb78432d48a970
Southclaws <southclawjk@gmail.com>
2017-12-04 13:05:16 +0000

    fixed typo with Resource json field name

--- rook/rook.go
+++ rook/rook.go
@@ -60,7 +60,7 @@ type PackageMeta struct {
 type Resource struct {
 	Name     string            `json:"name"`     // filename pattern of the resource
 	Platform string            `json:"platform"` // target platform, if empty the resource is always used but if this is set and does not match the runtime OS, the resource is ignored
-	Archive  bool              `json:"release"`  // is this resource an archive file or just a single file?
+	Archive  bool              `json:"archive"`  // is this resource an archive file or just a single file?
 	Includes []string          `json:"includes"` // if archive: paths to directories containing .inc files for the compiler
 	Plugins  []string          `json:"plugins"`  // if archive: paths to plugin binaries, either .so or .dll
 	Files    map[string]string `json:"files"`    // if archive: path-to-path map of any other files, keys are paths inside the archive and values are extraction paths relative to the sampctl working directory
