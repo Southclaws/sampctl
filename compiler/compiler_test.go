abbd3927be2eae9db13aa4e53bcb9171db00aa34
Southclaws <southclawjk@gmail.com>
2017-12-10 22:37:30 +0000

    reorganised compiler tests

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -23,18 +23,31 @@ func TestCompileSource(t *testing.T) {
 			util.FullPath("./tests/cache"),
 			Config{
 				WorkingDir: ".",
-				Input:      "./tests/compile/compile_test.pwn",
-				Output:     "./tests/compile/compile_test.amx",
+				Input:      "./tests/valid.pwn",
+				Output:     "./tests/valid.amx",
 				Includes:   []string{},
 				Version:    "3.10.4",
 			}}, false},
+		{"invalid", args{
+			util.FullPath("./tests/cache"),
+			Config{
+				WorkingDir: ".",
+				Input:      "./tests/invalid.pwn",
+				Output:     "./tests/invalid.amx",
+				Includes:   []string{},
+				Version:    "3.10.4",
+			}}, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := CompileSource(".", tt.args.cacheDir, tt.args.config); (err != nil) != tt.wantErr {
-				t.Errorf("CompileSource() error = %v, wantErr %v", err, tt.wantErr)
+			err := CompileSource(".", tt.args.cacheDir, tt.args.config)
+
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.True(t, util.Exists(tt.args.config.Output))
 			}
-			assert.True(t, util.Exists("./tests/compile/compile_test.amx"))
 		})
 	}
 }
---
+++ compiler/tests/invalid.pwn
@@ -0,0 +1,6 @@
+m ain() {
+    new a;
+    for(new i; i < 10; ++i) {
+        a++;
+    }
+}
