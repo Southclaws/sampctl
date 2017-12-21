47f764e5d6e7ed669380c36788c3c30e5abef504
Southclaws <southclawjk@gmail.com>
2017-12-21 16:59:25 +0000

    added tests for fcnpc plugin ensure

--- runtime/ensure_plugins_test.go
+++ runtime/ensure_plugins_test.go
@@ -77,6 +77,24 @@ func TestEnsureVersionedPlugin(t *testing.T) {
 				},
 			},
 		}, []string{"plugins/mysql.dll"}, []types.Plugin{"mysql"}, false},
+		{"fcnpc-linux", args{
+			types.Runtime{
+				WorkingDir: "./tests/ensure/fcnpc-linux",
+				Platform:   "linux",
+				Plugins: []types.Plugin{
+					"ziggi/FCNPC",
+				},
+			},
+		}, []string{"plugins/FCNPC.so"}, []types.Plugin{"FCNPC"}, false},
+		{"fcnpc-windows", args{
+			types.Runtime{
+				WorkingDir: "./tests/ensure/fcnpc-windows",
+				Platform:   "windows",
+				Plugins: []types.Plugin{
+					"ziggi/FCNPC",
+				},
+			},
+		}, []string{"plugins/FCNPC.dll"}, []types.Plugin{"FCNPC"}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
