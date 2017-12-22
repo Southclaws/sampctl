92e1222e38a0546993676c81b59b5d15295c32b4
Southclaws <southclawjk@gmail.com>
2017-12-22 16:26:47 +0000

    typo in tests

--- runtime/ensure_plugins_test.go
+++ runtime/ensure_plugins_test.go
@@ -135,53 +135,6 @@ func TestEnsurePlugins(t *testing.T) {
 	}
 }
 
-func TestGetPluginRemotePackage(t *testing.T) {
-	type args struct {
-		meta versioning.DependencyMeta
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantPkg types.Package
-		wantErr bool
-	}{
-		{"streamer", args{versioning.DependencyMeta{"samp-incognito", "samp-streamer-plugin", "", ""}}, types.Package{
-			DependencyMeta: versioning.DependencyMeta{
-				User: "samp-incognito",
-				Repo: "samp-streamer-plugin",
-			},
-			Resources: []types.Resource{
-				{
-					Name:     "^samp-streamer-plugin-(.*).zip$",
-					Platform: "linux",
-					Archive:  true,
-					Includes: []string{"pawno/include"},
-					Plugins:  []string{"plugins/streamer.so"},
-				},
-				{
-					Name:     "^samp-streamer-plugin-(.*).zip$",
-					Platform: "windows",
-					Archive:  true,
-					Includes: []string{"pawno/include"},
-					Plugins:  []string{"plugins/streamer.dll"},
-				},
-			},
-		}, false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			gotPkg, err := GetPluginRemotePackage(tt.args.meta)
-			if tt.wantErr {
-				assert.Error(t, err)
-			} else {
-				assert.NoError(t, err)
-			}
-
-			assert.Equal(t, tt.wantPkg, gotPkg)
-		})
-	}
-}
-
 func TestEnsurePluginsCache(t *testing.T) {
 	type args struct {
 		cfg types.Runtime
