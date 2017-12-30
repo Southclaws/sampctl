db7baf3c0d443ea59433a7e0ac5cce033e1a61b8
Southclaws <southclawjk@gmail.com>
2017-12-30 21:20:42 +0000

    moved release asset downloader to download package, made more generic

--- download/download.go
+++ download/download.go
@@ -2,21 +2,20 @@
 package download
 
 import (
-	"archive/tar"
-	"archive/zip"
-	"compress/gzip"
-	"compress/zlib"
-	"io"
+	"context"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"regexp"
 
+	"github.com/google/go-github/github"
 	"github.com/minio/go-homedir"
 	"github.com/pkg/errors"
 
 	"github.com/Southclaws/sampctl/util"
+	"github.com/Southclaws/sampctl/versioning"
 )
 
 // ExtractFunc represents a function responsible for extracting a set of files from an archive to
@@ -84,194 +83,39 @@ func FromNet(url, cacheDir, filename string) (result string, err error) {
 	return
 }
 
-// Untar takes a destination path and a reader; a tar reader loops over the tarfile
-// creating the file structure at 'dst' along the way, and writing any files
-// from https://medium.com/@skdomino/taring-untaring-files-in-go-6b07cf56bc07
-func Untar(src, dst string, paths map[string]string) (err error) {
-	r, err := os.Open(src)
-	if err != nil {
-		return errors.Wrap(err, "failed to open archive")
-	}
-	defer func() {
-		if err := r.Close(); err != nil {
-			panic(err)
-		}
-	}()
+// ReleaseAssetByPattern downloads a resource file, which is a GitHub release asset
+func ReleaseAssetByPattern(meta versioning.DependencyMeta, matcher *regexp.Regexp, platform, dir, cacheDir string) (filename string, err error) {
+	var (
+		client = github.NewClient(nil)
+		asset  *github.ReleaseAsset
+		assets []string
+	)
 
-	var tr *tar.Reader
-
-	gz, err := gzip.NewReader(r)
-	if err != nil {
-		zl, err := zlib.NewReader(r)
-		if err != nil {
-			return errors.Wrap(err, "failed to create new zlib reader after failed attempt at gzip")
-		}
-		defer func() {
-			if err := zl.Close(); err != nil {
-				panic(err)
-			}
-		}()
-		tr = tar.NewReader(zl)
+	var release *github.RepositoryRelease
+	if meta.Version == "" {
+		release, _, err = client.Repositories.GetLatestRelease(context.Background(), meta.User, meta.Repo)
 	} else {
-		defer func() {
-			if err := gz.Close(); err != nil {
-				panic(err)
-			}
-		}()
-		tr = tar.NewReader(gz)
-	}
-
-	var header *tar.Header
-loop:
-	for {
-		header, err = tr.Next()
-		switch {
-		// if no more files are found return
-		case err == io.EOF:
-			return nil
-
-		// return any other error
-		case err != nil:
-			break loop
-
-		// if the header is nil, just skip it (not sure how this happens)
-		case header == nil:
-			continue
-		}
-
-		// search by regular expression
-
-		var (
-			source string
-			target string
-			match  *regexp.Regexp
-			found  bool
-		)
-
-		for source, target = range paths {
-			match, err = regexp.Compile(source)
-			if err != nil {
-				if header.Name == source {
-					found = true
-					break
-				}
-			} else {
-				if match.MatchString(header.Name) {
-					found = true
-					break
-				}
-			}
-		}
-
-		if !found {
-			continue
-		}
-
-		// if the target is not absolute, make relative to destination dir
-		if !filepath.IsAbs(target) {
-			target = filepath.Join(dst, target)
-		}
-
-		// the following switch could also be done using fi.Mode(), not sure if there
-		// a benefit of using one vs. the other.
-		// fi := header.FileInfo()
-
-		if header.Typeflag == tar.TypeReg {
-			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
-			if err != nil {
-				return errors.Wrap(err, "failed to open extract target file")
-			}
-			defer func() {
-				if err := f.Close(); err != nil {
-					panic(err)
-				}
-			}()
-
-			// copy over contents
-			if _, err := io.Copy(f, tr); err != nil {
-				return errors.Wrap(err, "failed to copy contents to extract target file")
-			}
-		}
+		release, _, err = client.Repositories.GetReleaseByTag(context.Background(), meta.User, meta.Repo, meta.Version)
 	}
 	if err != nil {
-		err = errors.Wrap(err, "unhandled error while parsing archive")
+		return
 	}
-	return
-}
 
-// Unzip will un-compress a zip archive, moving all files and folders to an output directory.
-// from: https://golangcode.com/unzip-files-in-go/
-func Unzip(src, dst string, paths map[string]string) (err error) {
-	r, err := zip.OpenReader(src)
-	if err != nil {
-		return err
+	for _, a := range release.Assets {
+		if matcher.MatchString(*a.Name) {
+			asset = &a
+			break
+		}
+		assets = append(assets, *a.Name)
 	}
-	defer func() {
-		if err := r.Close(); err != nil {
-			panic(err)
-		}
-	}()
-
-	for _, f := range r.File {
-		var (
-			source string
-			target string
-			match  *regexp.Regexp
-			found  bool
-		)
-
-		for source, target = range paths {
-			match, err = regexp.Compile(source)
-			if err != nil {
-				return
-			}
-
-			if match.MatchString(f.Name) {
-				found = true
-				break
-			}
-		}
-
-		if !found {
-			continue
-		}
-
-		// if the target is not absolute, make relative to destination dir
-		if !filepath.IsAbs(target) {
-			target = filepath.Join(dst, target)
-		}
-
-		rc, err := f.Open()
-		if err != nil {
-			return err
-		}
-		defer func() {
-			if err := rc.Close(); err != nil {
-				panic(err)
-			}
-		}()
-
-		if !f.FileInfo().IsDir() {
-			err = os.MkdirAll(filepath.Dir(target), os.ModePerm)
-			if err != nil {
-				return err
-			}
-
-			f, err := os.OpenFile(target, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-			if err != nil {
-				return err
-			}
-			defer func() {
-				if err := f.Close(); err != nil {
-					panic(err)
-				}
-			}()
-
-			_, err = io.Copy(f, rc)
-			if err != nil {
-				return err
-			}
-		}
+	if asset == nil {
+		err = errors.Errorf("resource matcher '%s' does not match any release assets from '%v'", matcher, assets)
+		return
 	}
+
+	u, _ := url.Parse(*asset.BrowserDownloadURL)
+	outputFile := filepath.Join(dir, filepath.Base(u.Path))
+
+	filename, err = FromNet(*asset.BrowserDownloadURL, cacheDir, outputFile)
 	return
 }
---
+++ download/extract.go
@@ -0,0 +1,206 @@
+package download
+
+import (
+	"archive/tar"
+	"archive/zip"
+	"compress/gzip"
+	"compress/zlib"
+	"io"
+	"os"
+	"path/filepath"
+	"regexp"
+
+	"github.com/pkg/errors"
+)
+
+// Untar takes a destination path and a reader; a tar reader loops over the tarfile
+// creating the file structure at 'dst' along the way, and writing any files
+// from https://medium.com/@skdomino/taring-untaring-files-in-go-6b07cf56bc07
+func Untar(src, dst string, paths map[string]string) (err error) {
+	r, err := os.Open(src)
+	if err != nil {
+		return errors.Wrap(err, "failed to open archive")
+	}
+	defer func() {
+		if err := r.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	var tr *tar.Reader
+
+	gz, err := gzip.NewReader(r)
+	if err != nil {
+		zl, err := zlib.NewReader(r)
+		if err != nil {
+			return errors.Wrap(err, "failed to create new zlib reader after failed attempt at gzip")
+		}
+		defer func() {
+			if err := zl.Close(); err != nil {
+				panic(err)
+			}
+		}()
+		tr = tar.NewReader(zl)
+	} else {
+		defer func() {
+			if err := gz.Close(); err != nil {
+				panic(err)
+			}
+		}()
+		tr = tar.NewReader(gz)
+	}
+
+	var header *tar.Header
+loop:
+	for {
+		header, err = tr.Next()
+		switch {
+		// if no more files are found return
+		case err == io.EOF:
+			return nil
+
+		// return any other error
+		case err != nil:
+			break loop
+
+		// if the header is nil, just skip it (not sure how this happens)
+		case header == nil:
+			continue
+		}
+
+		// search by regular expression
+
+		var (
+			source string
+			target string
+			match  *regexp.Regexp
+			found  bool
+		)
+
+		for source, target = range paths {
+			match, err = regexp.Compile(source)
+			if err != nil {
+				if header.Name == source {
+					found = true
+					break
+				}
+			} else {
+				if match.MatchString(header.Name) {
+					found = true
+					break
+				}
+			}
+		}
+
+		if !found {
+			continue
+		}
+
+		// if the target is not absolute, make relative to destination dir
+		if !filepath.IsAbs(target) {
+			target = filepath.Join(dst, target)
+		}
+
+		// the following switch could also be done using fi.Mode(), not sure if there
+		// a benefit of using one vs. the other.
+		// fi := header.FileInfo()
+
+		if header.Typeflag == tar.TypeReg {
+			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+			if err != nil {
+				return errors.Wrap(err, "failed to open extract target file")
+			}
+			defer func() {
+				if err := f.Close(); err != nil {
+					panic(err)
+				}
+			}()
+
+			// copy over contents
+			if _, err := io.Copy(f, tr); err != nil {
+				return errors.Wrap(err, "failed to copy contents to extract target file")
+			}
+		}
+	}
+	if err != nil {
+		err = errors.Wrap(err, "unhandled error while parsing archive")
+	}
+	return
+}
+
+// Unzip will un-compress a zip archive, moving all files and folders to an output directory.
+// from: https://golangcode.com/unzip-files-in-go/
+func Unzip(src, dst string, paths map[string]string) (err error) {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := r.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	for _, f := range r.File {
+		var (
+			source string
+			target string
+			match  *regexp.Regexp
+			found  bool
+		)
+
+		for source, target = range paths {
+			match, err = regexp.Compile(source)
+			if err != nil {
+				return
+			}
+
+			if match.MatchString(f.Name) {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			continue
+		}
+
+		// if the target is not absolute, make relative to destination dir
+		if !filepath.IsAbs(target) {
+			target = filepath.Join(dst, target)
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := rc.Close(); err != nil {
+				panic(err)
+			}
+		}()
+
+		if !f.FileInfo().IsDir() {
+			err = os.MkdirAll(filepath.Dir(target), os.ModePerm)
+			if err != nil {
+				return err
+			}
+
+			f, err := os.OpenFile(target, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer func() {
+				if err := f.Close(); err != nil {
+					panic(err)
+				}
+			}()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return
+}
