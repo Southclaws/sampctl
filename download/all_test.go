d9e23a6e046c047489ccd3291e19ba3e73bfbd00
Southclaws <southclawjk@gmail.com>
2018-02-25 11:36:06 +0000

    removed unnecessary checks

--- download/all_test.go
+++ download/all_test.go
@@ -13,10 +13,7 @@ import (
 var gh *github.Client
 
 func TestMain(m *testing.M) {
-	err := godotenv.Load("../.env", "../../.env")
-	if err != nil {
-		panic(err)
-	}
+	godotenv.Load("../.env", "../../.env")
 	gh = github.NewClient(oauth2.NewClient(context.Background(), oauth2.StaticTokenSource(&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")})))
 
 	os.Exit(m.Run())
