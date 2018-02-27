c8fdde1b0a36a2307a5c52473b3c8cbb7aee78a8
Southclaw <southclawjk@gmail.com>
2018-02-27 19:34:28 +0000

    finished first runtime unit test

--- runtime/run_test.go
+++ runtime/run_test.go
@@ -20,7 +20,8 @@ func TestRun(t *testing.T) {
 		wantOutput string
 		wantErr    bool
 	}{
-		{"bare", `----------
+		{"bare", `
+----------
 Loaded log file: "server_log.txt".
 ----------
 
@@ -28,42 +29,52 @@ SA-MP Dedicated Server
 ----------------------
 v0.3.7-R2, (C)2005-2015 SA-MP Team
 
+plugins = ""  (string)
 
 Server Plugins
 --------------
-	Loaded 0 plugins.
+ Loaded 0 plugins.
 
 
-Started server on port: 7777, with maxplayers: 50 lanmode is OFF.
+Started server on port: 7777, with maxplayers: 32 lanmode is OFF.
 
 
 Filterscripts
 ---------------
-	Loaded 0 filterscripts.
+  Loaded 0 filterscripts.
 
 AllowAdminTeleport() : function is deprecated. Please see OnPlayerClickMap()
 
 ----------------------------------
-	Bare Script
+  Bare Script
 
 ----------------------------------
 
-Number of vehicle models: 0`,
+Number of vehicle models: 0
+`,
 			false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 			defer cancel()
 
-			config := types.MergeRuntimeDefault(&types.Runtime{})
+			dir := util.FullPath(filepath.Join("./tests/run/", tt.name))
+
+			config, err := NewConfigFromEnvironment(dir)
+			if err != nil {
+				panic(err)
+			}
 			config.AppVersion = Version
 			config.Platform = runtime.GOOS
 			config.Version = "0.3.7"
 			config.Gamemodes = []string{tt.name}
-			config.WorkingDir = util.FullPath(filepath.Join("./tests/run/", tt.name))
+			config.WorkingDir = dir
 
-			GetServerPackage("http://files.sa-mp.com", "0.3.7", config.WorkingDir, runtime.GOOS)
+			err = Ensure(context.Background(), gh, &config, false, false)
+			if err != nil {
+				panic(err)
+			}
 
 			if runtime.GOOS == "darwin" {
 				config.Container = &types.ContainerConfig{
@@ -72,8 +83,10 @@ Number of vehicle models: 0`,
 			}
 
 			output := &bytes.Buffer{}
-			err := Run(ctx, *config, util.FullPath("./tests/cache"), output, nil)
-			assert.NoError(t, err)
+			err = Run(ctx, config, util.FullPath("./tests/cache"), output, nil)
+			if err.Error() != "received runtime error: failed to start server: exit status 1" {
+				assert.NoError(t, err)
+			}
 
 			gotOutput := output.String()
 			assert.Equal(t, tt.wantOutput, gotOutput)
