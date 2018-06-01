f44b46712cf87dad60cdab459a57731e28fc71a0
Southclaws <southclawjk@gmail.com>
2018-06-01 22:36:10 +0100

    fixed nil pointer crash on run

--- runtime/run_windows.go
+++ runtime/run_windows.go
@@ -12,7 +12,7 @@ func platformRun(cmd *exec.Cmd, w io.Writer, r io.Reader) (err error) {
 	cmd.Stdin = r
 	err = cmd.Run()
 	// process kill on windows: "exit status 1"
-	if err.Error() == "exit status 1" {
+	if err != nil && err.Error() == "exit status 1" {
 		err = nil
 	}
 	return
