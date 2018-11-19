4f2afaeef43e6ffc057aa570fc947d093cc3d98b
fr0st <Jon@frostgaming.com>
2018-11-19 04:31:02 -0600

    Fix two typos
    fix two typos

--- rook/load.go
+++ rook/load.go
@@ -121,7 +121,7 @@ func getPackageTag(dir string) (tag string) {
 	} else {
 		vtag, errInner := versioning.GetRepoCurrentVersionedTag(repo)
 		if errInner != nil {
-			// error information only needs to be printed wth --verbose
+			// error information only needs to be printed with --verbose
 			print.Verb("failed to get version information:", errInner)
 			// but we can let the user know that they should version their code!
 			print.Info("Package does not have any tags, consider versioning your code with: `sampctl package release`")
