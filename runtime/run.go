a3139d7d6c20e32cb2bf3217404845e1c5d9d40b
ADRFranklin <adrfranklin95@gmail.com>
2020-05-15 11:30:34 +0100

    Added support for runtime binaries paths

--- runtime/run.go
+++ runtime/run.go
@@ -45,7 +45,7 @@ func Run(
 		return RunContainer(ctx, cfg, cacheDir, passArgs, output, input)
 	}
 
-	binary := "./" + getServerBinary(cfg.Platform)
+	binary := "./" + getServerBinary(cacheDir, cfg.Version, cfg.Platform)
 	fullPath := filepath.Join(cfg.WorkingDir, binary)
 	print.Verb("starting", binary, "in", cfg.WorkingDir)
 
