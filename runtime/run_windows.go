3efdb739e853036b4e1ee3c64e06e34c9cbbaa39
JustMichael <adrfranklin95@gmail.com>
2022-11-27 18:35:46 +0000

    fix: output conversion for windows error codes

--- runtime/run_windows.go
+++ runtime/run_windows.go
@@ -1,3 +1,4 @@
+//go:build windows
 // +build windows
 
 package runtime
@@ -5,6 +6,11 @@ package runtime
 import (
 	"io"
 	"os/exec"
+	"strconv"
+	"strings"
+
+	"github.com/pkg/errors"
+	"rs3.io/go/mserr/ntstatus"
 )
 
 func platformRun(cmd *exec.Cmd, w io.Writer, r io.Reader) (err error) {
@@ -13,8 +19,22 @@ func platformRun(cmd *exec.Cmd, w io.Writer, r io.Reader) (err error) {
 	cmd.Stdin = r
 	err = cmd.Run()
 	// process kill on windows: "exit status 1"
-	if err != nil && err.Error() == "exit status 1" {
-		err = nil
+	if err != nil {
+		if err.Error() == "exit status 1" {
+			err = nil
+			return
+		}
+
+		if strings.Contains(err.Error(), "exit status") {
+			statusCodeStr := strings.Split(err.Error(), " ")[2]
+			statusCodeInt, innerError := strconv.ParseInt(statusCodeStr, 0, 64)
+			if innerError != nil {
+				return innerError
+			}
+			statusCode := ntstatus.NTStatus(uint32(statusCodeInt))
+			err = errors.Errorf("exit status %s\n", statusCode.String())
+		}
 	}
+
 	return
 }
