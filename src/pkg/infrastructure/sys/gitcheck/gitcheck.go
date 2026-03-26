23907cd2ac7e3490bdac7513c01e552a2e09b111
themxp <mattiastb@outlook.de>
2026-03-26 09:46:25 -0400

    no capital i guess?

--- src/pkg/infrastructure/sys/gitcheck/gitcheck.go
+++ src/pkg/infrastructure/sys/gitcheck/gitcheck.go
@@ -18,7 +18,7 @@ func RequireInstalled() error {
 		"git is not installed on your system.\n" +
 			"sampctl requires git to manage dependencies.\n" +
 			"install git using one of these methods:\n" +
-			"  1. winget install --id Git.Git -e --source winget\n" +
+			"  1. winget install --id git.git -e --source winget\n" +
 			"  2. scoop install git\n" +
 			"  3. download from https://git-scm.com",
 	)
